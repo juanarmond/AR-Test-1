edfb086f884224c28ba546067d0769dec2d8cc8b
Juan Armond <juan_armond@hotmail.com>
2018-06-13 10:21:27 +0100

    Fix Enabling Camera

--- AR Test 1/ViewController.swift
+++ AR Test 1/ViewController.swift
@@ -15,12 +15,17 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupScene()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setupConfiguration()
     }
     
     func setupScene() {
