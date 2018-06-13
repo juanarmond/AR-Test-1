6d4f604a5f23371ee9f66369b7ee4f118f26726e
Juan Armond <juan_armond@hotmail.com>
2018-06-13 07:58:49 +0100

    Initial Commit

---
+++ AR Test 1Tests/AR_Test_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  AR_Test_1Tests.swift
+//  AR Test 1Tests
+//
+//  Created by Juan Armond on 13/06/2018.
+//  Copyright © 2018 Juan Armond. All rights reserved.
+//
+
+import XCTest
+@testable import AR_Test_1
+
+class AR_Test_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
